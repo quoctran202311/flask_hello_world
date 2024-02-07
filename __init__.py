e6dde4003b0a815968811256461c13e7d85126f2
quoctran202311 <158566272+quoctran202311@users.noreply.github.com>
2024-02-07 23:56:10 +0100

    Update __init__.py - fiche client
    fiche client pour saisie

--- __init__.py
+++ __init__.py
@@ -48,6 +48,17 @@ def ReadBDD():
     
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
-  
+
+@app.route('/fiche_client/<int:post_id>')
+def Readfiche(post_id):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 if __name__ == "__main__":
   app.run(debug=True)
