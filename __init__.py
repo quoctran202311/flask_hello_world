012974017e0ad96a682d03bd1500ae8afed997a0
quoctran202311 <158566272+quoctran202311@users.noreply.github.com>
2024-02-08 10:05:46 +0100

    Update __init__.py - SearchClient
    Fiche pour rechercher un client sur nom

--- __init__.py
+++ __init__.py
@@ -60,5 +60,17 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
+@app.route('/fiche_client/')
+def Searchfiche():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    nom = input("Nom client a chercher: ");
+    cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom,))
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 if __name__ == "__main__":
   app.run(debug=True)
