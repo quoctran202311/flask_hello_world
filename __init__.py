ae964df972076106793fc861be9ae057a202d04b
quoctran202311 <158566272+quoctran202311@users.noreply.github.com>
2024-02-08 11:58:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template_string, render_template, jsonify
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask import json
 from urllib.request import urlopen
 import sqlite3
