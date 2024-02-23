53e615b8f3ddaf738d16c2578b5ca95a7aee4a35
quoctran202311 <158566272+quoctran202311@users.noreply.github.com>
2024-02-23 16:36:00 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -40,7 +40,7 @@ def mongraphiquehisto():
 def mongraphiquecol():
     return render_template("graphiquecol.html")
 
-@app.route("/consultation/")
+@app.route("/t_consultation/")
 def ReadBDD():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
@@ -51,7 +51,7 @@ def ReadBDD():
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_client/<int:post_id>')
+@app.route('/t_fiche_client/<int:post_id>')
 def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
@@ -62,7 +62,7 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_clientn/<string:nom>')
+@app.route('/t_fiche_clientn/<string:nom>')
 def ReadFicheNom(nom):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
@@ -73,7 +73,7 @@ def ReadFicheNom(nom):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/chercher_client', methods=['GET', 'POST'])
+@app.route('/t_chercher_client', methods=['GET', 'POST'])
 def chercherClient():
     data = []  # Define data as an empty list
     if request.method == 'POST':
@@ -98,7 +98,7 @@ def chercherClient():
     return render_template('search_data.html', data=data)
 
 
-@app.route('/ajouter_client/', methods=['GET', 'POST'])
+@app.route('/t_ajouter_client/', methods=['GET', 'POST'])
 def ajouterClient():
     if request.method == 'POST':
         # Récupérer les données du formulaire
@@ -188,6 +188,32 @@ def Afficher_Produit():
     
     # Rendre le template HTML et transmettre les données
     return render_template('mspr_Afficher_Produit.html', data=data)
+
+
+@app.route('/mspr_Ajouter_Client', methods=['GET', 'POST'])
+def Ajouter_Client():
+    if request.method == 'POST':
+        # Récupérer les données du formulaire
+        nom = request.form['nom']
+        prenom = request.form['prenom']
+        email = request.form['email']
+        telephone = request.form['telephone']
+
+        # Insérer les données dans la base de données (ici, je suppose que tu as une table 'clients')
+        conn = sqlite3.connect('weeat.db')
+        cursor = conn.cursor()
+        if conn is not None:
+            cursor.execute('INSERT INTO clients (Nom, Prenom, Email, Telephone) VALUES (?, ?, ?)', (nom, prenom, email, telephone))
+            conn.commit()
+            conn.close()
+        else:
+            return 'Erreur de connexion à la base de données'
+
+        # Rediriger vers la page de consultation des clients après l'ajout
+        return redirect(url_for('/'))
+
+    # Si la méthode est GET, simplement rendre le template du formulaire
+    return render_template('mspr_Ajouter_Client.html')
 
 
 if __name__ == "__main__":
