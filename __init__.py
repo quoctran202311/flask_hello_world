0eed99f3e206c45f52c13052730dbb67630de848
quoctran202311 <158566272+quoctran202311@users.noreply.github.com>
2024-02-28 12:41:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -98,8 +98,9 @@ def chercher_client():
 
 @app.route('/t_ajouter_client', methods=['GET', 'POST'])
 def ajouter_client():
-    # data = []  # Define data as an empty list
+    data = []  # Define data as an empty list
     if request.method == 'POST':
+        data = []  # Define data as an empty list
         # Récupérer les données du formulaire
         nom = request.form['nom']
         prenom = request.form['prenom']
@@ -111,8 +112,8 @@ def ajouter_client():
         if conn is not None:
             cursor.execute('INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)', (nom, prenom, adresse))
             conn.commit()
-            cursor.execute('SELECT * FROM clients;')
-            data = cursor.fetchall()
+            # cursor.execute('SELECT * FROM clients;')
+            # data = cursor.fetchall()
             conn.close()
             # Rendre le template HTML et transmettre les données
             return render_template('read_data.html', data=data)
